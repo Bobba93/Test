fb7970c4092371b319b6fbb1428ada7b3aeb5f0d
James Hodgson <jameshodgson1993@hotmail.co.uk>
2017-07-25 17:48:41 +0100

    Hello World

---
+++ gittest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by James Hodgson on 25/07/2017.
+//  Copyright © 2017 James Hodgson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print ("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
